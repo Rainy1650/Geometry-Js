7e6c20039d82affe6555903545953b23c7569e3e
Rainy1650 <anamika16500@gmail.com>
2024-11-01 20:39:24 +0600

    geometric

--- script/Triangle-Calculate.js
+++ script/Triangle-Calculate.js
@@ -1,14 +1,15 @@
 function triangleCalculate(){
     const triangleBaseArea = document.getElementById('triangle-base')
     const triangleBaseText = triangleBaseArea.value;
-    const base = parseFloat( triangleBaseText)
+    const base = parseFloat( triangleBaseText);
     console.log(base);
 // get hight value
     const triangleHightInput = document.getElementById('triangle-hight');
     const triangleHightText = triangleHightInput.value;
-    const height= parseFloat(triangleHightText)
+    const height= parseFloat(triangleHightText);
     console.log(height);
+
+//caculate triangle  base and hight
+const area = 0.5 * base * height;
+console.log(area)
 }
-//caculate triangle and hight
-const area = (0.5 * base * height);
-console.log( area)
--- script/rectangle area.js
+++ script/rectangle area.js
@@ -1,3 +1,23 @@
 function calculateArea(){
-    console.log( 'calculate the react area')
+    const lengthInput = document.getElementById('rectangle-l');
+    const lengthinputText = lengthInput.value;
+    const length = parseFloat(lengthinputText);
+    console.log(length);
+
+// get width of the rectangle
+const widthInput = document.getElementById('rectangle-w');
+const widthinputText = widthInput.value;
+const width = parseFloat(widthinputText);
+console.log(width);
+
+
+// calculate the length and width
+const area = width * length;
+console.log(area)
+
+
+
+
+
+
 }
